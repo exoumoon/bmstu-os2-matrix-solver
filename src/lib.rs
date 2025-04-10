6f68115a28180e35636c30f5d99e776c13cf61f2
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-04-10 22:30:26 +0300

    chore: Remove rotten misleading comment

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,6 @@ impl Spmv {
         let result: Vec<f64> = matrix
             .row_iter()
             .map(|row| {
-                /* NOTE: Код в этом блоке выполняется параллельно */
                 row.values()
                     .iter()
                     .zip(row.col_indices())
